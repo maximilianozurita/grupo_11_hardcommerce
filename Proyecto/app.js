8696a75ec246012c93f3039e4c40e6ef2a92abc4
Jeremias256 <gab.menacho@gmail.com>
2021-05-15 23:05:00 -0300

    modificaciones pequeñas

--- Proyecto/app.js
+++ Proyecto/app.js
@@ -10,4 +10,4 @@ const publicPath=path.resolve(__dirname, "./public");
 
 app.use(express.static(publicPath));
 
-app.get ("/",(req,res)=>{res.sendFile(path.resolve(__dirname, "./views/index.html"))})
+app.get ("/",(req,res)=>{res.sendFile(path.resolve(__dirname, "./views/productDetail.html"))})

a81075ba1f813475bf1556117114efdf3e58cda9
Jeremias256 <gab.menacho@gmail.com>
2021-05-18 01:22:33 -0300

    actualizar links

--- Proyecto/app.js
+++ Proyecto/app.js
@@ -10,4 +10,8 @@ const publicPath=path.resolve(__dirname, "./public");
 
 app.use(express.static(publicPath));
 
-app.get ("/",(req,res)=>{res.sendFile(path.resolve(__dirname, "./views/productDetail.html"))})
+app.get ("/",(req,res)=>{res.sendFile(path.resolve(__dirname, "./views/index.html"))})
+app.get ("/login",(req,res)=>{res.sendFile(path.resolve(__dirname, "./views/login.html"))})
+app.get ("/register",(req,res)=>{res.sendFile(path.resolve(__dirname, "./views/register.html"))})
+app.get ("/productCart",(req,res)=>{res.sendFile(path.resolve(__dirname, "./views/productCart.html"))})
+app.get ("/productoDetail",(req,res)=>{res.sendFile(path.resolve(__dirname, "./views/productDetail.html"))})

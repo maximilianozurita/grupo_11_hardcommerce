2f7c2d74322a03c60c26332f7143598fcde22545
jeremias256 <gab.menacho@gmail.com>
2021-05-26 10:56:29 -0300

    rutas actualizadas

--- Proyecto/app.js
+++ Proyecto/app.js
@@ -1,14 +1,16 @@
-const express=require("express");
+const express=require('express');
 const app=express();
-const path=require("path");
+const path=require('path');
 
 //const publicPath=path.resolve(__dirname, "./public");
 //app.use(express.static(publicPath));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 
 
-app.get ("/",(req,res)=>{res.sendFile(path.resolve("views/index.html"))})
+app.get ("/",(req,res)=>{
+    res.sendFile(path.resolve('views/index.html'))
+})
 app.get ("/login",(req,res)=>{res.sendFile(path.resolve("views/login.html"))})
 app.get ("/register",(req,res)=>{res.sendFile(path.resolve("views/register.html"))})
 app.get ("/productCart",(req,res)=>{res.sendFile(path.resolve("views/productCart.html"))})
@@ -16,11 +18,10 @@ app.get ("/productCart",(req,res)=>{res.sendFile(path.resolve("views/productCart
 //app.get ("/productDetail",(req,res)=>{res.sendFile(path.resolve(__dirname, "./views/productDetail.html"))})
 
 //productos
+const productsRoutes = require('./routes/productsRoutes');
 
-const productsRoutes = require("./routes/productsRoutes");
-
-app.use("/products", productsRoutes);
+app.use('/products', productsRoutes)
 
 app.listen(3000,()=>{
-    console.log("Servidor ejecutado");
+    console.log('Servidor ejecutado');
 });
--- Proyecto/controllers/productsController.js
+++ Proyecto/controllers/productsController.js
@@ -1,12 +1,13 @@
 const path = require('path')
 
-const productsController = {
+const productsController = 
+{
     listaDeArticulos: (req, res) => {
         res.sendFile(path.resolve('views/listaDeArticulos.html'))
     },
     detail: (req, res) => {
-        res.sendFile(path.resolve('views/productDetail.html'))
+        res.sendFile(path.resolve('views/product-detail.html'))
     },
 }
 
-module.exports = productsController;
+module.exports = productsController

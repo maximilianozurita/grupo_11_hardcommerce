5c8145bc98012ce9649784a1ba8b8f61af18dc2e
jeremias256 <gab.menacho@gmail.com>
2021-05-26 22:31:15 -0300

    actualización rutas,header y footer

--- Proyecto/app.js
+++ Proyecto/app.js
@@ -8,6 +8,9 @@ app.use(express.static('public'));
 
 
 
+/*app.get ("/",(req,res)=>{
+    res.sendFile(path.resolve('views/index.html'))
+})*/
 app.get ("/",(req,res)=>{
     res.sendFile(path.resolve('views/index.html'))
 })

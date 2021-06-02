ea63364a6342657bc57062171b07a08252bfa659
jeremias256 <gab.menacho@gmail.com>
2021-06-01 23:37:02 -0300

    1/6

--- Proyecto/routes/productsRoutes.js
+++ Proyecto/routes/productsRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const productsRoutes = express.Router()
-
 const productsController = require('../controllers/productsController')
 
 productsRoutes.get('/listOfArticles', productsController.listaDeArticulos)

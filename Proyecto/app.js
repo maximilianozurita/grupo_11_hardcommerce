bc2ca1cf3cf15436b2eaf999f7e0ce263dca16d6
maximilianozurita <maxi.zurita.seb@gmail.com>
2021-06-06 18:35:01 -0300

    Se incorporan CRUD a products

--- Proyecto/app.js
+++ Proyecto/app.js
@@ -1,18 +1,31 @@
 const express=require('express');
-const app=express();
 const path=require('path');
-app.set("view engine", "ejs");
+const method = require('method-override');
+const productsModels=require(__dirname + "/models/productsModels");
+const productModel=require ("./models/productsModels");
+
+//Guarda los modulos de express en app.
+const app=express();
+
+//Ejecuta el metodo para utilizar method
+app.use(method('_method'));
+
+//Setea para utilizar EJS con una ruta en views
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// No olvidarse esto para que la data se envie correctamente desde un formulario
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 
-app.set('view engine', 'ejs')
-app.set('views', './views')
-
+//ESTABLECIENDO RUTAS ESTATICAS EN PUBLIC
 //const publicPath=path.resolve(__dirname, "./public");
 //app.use(express.static(publicPath));
-app.use(express.static('public'));
+//app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, './public')));
 
-const productModel=require ("./models/productsModels")
-//TRAER HTML con sendFile (ya no se usa, ahora se usar el metodo render para traer ejs)
+//TRAE EL HTML con sendFile (ya no se usa, ahora se usar el metodo render para traer ejs)
 /*app.get ("/",(req,res)=>{
     res.sendFile(path.resolve('views/index.html'))
 })*/
@@ -21,22 +34,29 @@ const productModel=require ("./models/productsModels")
 //app.get ("/productCart",(req,res)=>{res.sendFile(path.resolve("views/productCart.html"))})
 //app.get ("/productDetail",(req,res)=>{res.sendFile(path.resolve(__dirname, "./views/productDetail.html"))})
 
-//Render del HOME
+//Render del HOME sin routes y controller.
 app.get('/', (req, res) => {
-    const productList=productModel.findAll();
-    res.render('main/index',{productList: productList})})
-//Render de carga y edicion de productos
-app.get('/productEdition', (req, res) => {res.render('main/productEdition')})
+    const productList=productsModels.findAll();
+    res.render('main/index',{productList})
+})
+
 //Render de productos
 const productsRoutes = require('./routes/productsRoutes');
 app.use('/products', productsRoutes)
 
 //Render de users
 const userRoutes = require('./routes/userRoutes');
+const { dirname } = require('path');
 app.use('/user', userRoutes)
 
 
+//Si no encuentra la pagina porque el URL es incorrecto redirige a la pagina "not-found".
+app.use((req,res,next)=>{
+    res.status(404).render("./main/not-found");
+})
 
+
+//Abre el servidor
 app.listen(3000,()=>{
     console.log('Servidor ejecutado');
 });
--- Proyecto/controllers/productsController.js
+++ Proyecto/controllers/productsController.js
@@ -1,10 +1,11 @@
 const path = require('path')
+const productsModels=require ("../models/productsModels")
 
-const productsController = 
-{
+const productsController = {
     listaDeArticulos: (req, res) => {
-        //res.sendFile(path.resolve('views/products/listaDeArticulos.html'))
-        res.render('products/listOfArticles',)
+        const productList=productsModels.findAll();
+        //res.sendFile(path.resolve('views/products/listaDeArticulos.html'))-->>En caso de enviar el HTML.
+        res.render('products/listOfArticles',{productList})
     },
     detail: (req, res) => {
         res.render('products/productDetail')
@@ -12,6 +13,42 @@ const productsController =
     cart: (req,res)=>{
         res.render('products/productCart')
     },
+    products:(req, res) => {
+        const productList=productsModels.findAll()
+        res.render('products/products',{productList})
+    },
+    formNew: (req,res)=>{
+        res.render("products/productCreate")
+    },
+    store: (req,res)=>{
+        const product={
+            "id": req.body.id,
+            "name": req.body.name,
+            "description": req.body.description,
+            "category": req.body.category,
+            "precio": req.body.price,
+            "cuotas": req.body.cuotas
+        }
+        const productCreated=productsModels.create(product);
+
+        res.redirect("/products/");
+    },
+    edit:(req,res)=>{
+        const productToEdit=productsModels.findByPk(req.params.id);
+        res.render("products/productEdition",{productToEdit})
+    },
+    update: (req,res)=>{
+        const data=req.body;
+        const id=req.params.id;
+        productsModels.update(data,id);
+        res.redirect("/products/");
+    },
+    destroy:(req,res)=>{
+        const id=req.params.id;
+        productsModels.destroy(id);
+
+        res.redirect("/products/");
+    }
 }
 
 module.exports = productsController

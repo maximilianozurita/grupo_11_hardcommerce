db0b16dc311448f4f2e0ece5bd7e07661e3a5510
maximilianozurita <maxi.zurita.seb@gmail.com>
2021-06-09 22:52:57 -0300

    crud de imagenes

--- Proyecto/routes/productsRoutes.js
+++ Proyecto/routes/productsRoutes.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const productsRoutes = express.Router()
-
+const path=require("path")
 const productsController = require('../controllers/productsController')
+const multer=require("multer")
+
+const storage=multer.diskStorage({
+    destination: (req,file,cb)=>{
+        cb(null,path.join(__dirname, "../public/images/imgProducts"))
+    },
+    filename:(req,file,cb)=>{
+        const newFileName="product-"+Date.now()+path.extname(file.originalname);
+        cb(null,newFileName);
+    }
+})
+const upload=multer({storage:storage});
 
 //--------------------VISTA CLIENTES--------------------------------
 
 productsRoutes.get('/listOfArticles', productsController.listaDeArticulos) //-->>listado de productos
 productsRoutes.get('/detail/:id', productsController.detail)               //-->>Detalle de producto seleccionado.
-productsRoutes.get('/cart',productsController.cart)                        //-->>Carrito de compras.
+productsRoutes.get('/cart',productsController.cart)                    //-->>Carrito de compras.
 
 //-------------------VISTA ADMINISTRADOR------------------------------
 
@@ -15,13 +27,13 @@ productsRoutes.get("/",productsController.products) //-->>LISTADO DE PRODUCTOS
 
 //Create:
 productsRoutes.get("/productCreate", productsController.formNew) //-->> Pagina de creacion.
-productsRoutes.post("/productCreate",productsController.store)
-//Edition
+productsRoutes.post("/productCreate",upload.single("productImage"), productsController.store)
+
+//Update
 productsRoutes.get("/productEdition/:id", productsController.edit)
-productsRoutes.put("/productEdition/:id", productsController.update)
+productsRoutes.put("/productEdition/:id",upload.single("productImage"), productsController.update)
 
 productsRoutes.delete("/:id", productsController.destroy)
-
 
 
 module.exports = productsRoutes

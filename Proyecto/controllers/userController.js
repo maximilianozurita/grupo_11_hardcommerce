79086b5f3e672293801c289b98f7bc1acf46a6ab
jeremias256 <gab.menacho@gmail.com>
2021-06-15 23:13:54 -0300

    multer

--- Proyecto/controllers/userController.js
+++ Proyecto/controllers/userController.js
@@ -22,14 +22,18 @@ const userController = {
     store: (req, res) => {
     
         const {name, lastName, email, password, cell} = req.body;
-            
+        
+        const { file } = req;
+        const imagen = file.filename;
+
         const user = 
         {
             name:name,
             lastName:lastName,
             email:email,
             password:password,
-            cell:cell
+            cell:cell,
+            imagen: "/images/imgUser/" + imagen,
         }
         const userCreated = usersModels.create(user);
         res.redirect('/user/userDetail/' + userCreated.id);
@@ -41,9 +45,25 @@ const userController = {
     update: (req, res) => {
         const data = req.body;
         const { id } = req.params;
+        // el usuario original y su imagen
+        const userOriginal = usersModels.findByPk(id);
+
+        // dentro de req.file va a venir la información del archivo
+        const { file } = req
+
+        /* Si viene una imagen nueva, cargar la imagen nueva
+        sino poner la original */
+        let imagen
+
+        if (file) {
+            imagen = '/images/imgUser/' + file.filename
+        } else {
+            imagen = userOriginal.imagen
+        }
+
+        data.imagen = imagen
 
         usersModels.update(data, id);
-
         res.redirect('/user/listOfUsers');
     },
     destroy: (req, res) => {
--- Proyecto/routes/userRoutes.js
+++ Proyecto/routes/userRoutes.js
@@ -1,6 +1,35 @@
 const express = require('express');
 const userRoutes = express.Router();
 const userController = require('../controllers/userController');
+const multer = require('multer')
+const path = require('path')
+
+// destino donde guardar el archivo
+// nombre del archivo
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        // guardamos el destino de la carpeta absoluta
+        const detinationPath = path.join(__dirname, '../public/images/imgUser')
+        // llamamos al callback con error (null) y el path de donde guardaría el archivo
+        cb(null, detinationPath)
+    },
+    filename: (req, file, cb) => {
+        console.log('file', file)
+        // El nombre del archivo original es: file.originalname
+        const extension = path.extname(file.originalname) // .jpg
+
+        // generamos un identificador único a partir de la fecha
+        const now = Date.now() // 32173821637218631
+
+        // generar un nombre para nuestro archivo
+        //const filename = `${now}${extension}`
+        const filename = now + extension
+        
+        // ejecutamos callback con null (error) y el nombre del archivo
+        cb(null, filename)
+    },
+})
+const upload = multer({ storage })
 
 userRoutes.get('/login', userController.login);
 
@@ -8,10 +37,13 @@ userRoutes.get('/listOfUsers',userController.listOfUsers);
 userRoutes.get('/userDetail/:id', userController.detail);
 
 userRoutes.get("/register", userController.formNew);
-userRoutes.post('/register', userController.store);
+//userRoutes.post('/register', userController.store);
+userRoutes.post('/register', upload.single('imagen'), userController.store);
 
 userRoutes.get('/editUsers/:id', userController.edit);
-userRoutes.put('/:id', userController.update);
+
+//userRoutes.put('/:id', userController.update);
+userRoutes.put('/:id', upload.single('imagen'), userController.update);
 
 userRoutes.delete("/:id", userController.destroy);
 

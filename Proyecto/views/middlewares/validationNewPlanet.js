22ab2f11ce55b4ff6af5d3b28fc4598befdeec99
Martin Mila <mmila83@hotmail.com>
2021-06-19 21:11:35 -0300

    se genera el middleware    '" validationNewPlanet "

---
+++ Proyecto/views/middlewares/validationNewPlanet.js
@@ -0,0 +1,70 @@
+const { body } = require ('express-validator');
+const path = require ('path');
+
+
+const validationNewPlanet = [
+
+    body('name')
+        .notEmpty()
+        .withMessage('Por favor ingrese su nombre')  
+        .bail()
+        //
+        .isLength({ min:3 })
+        .withMessage('Por favor un nombre más largo'),
+        //como es la ultima no usamos bail.
+
+    body('lastname')
+        .notEmpty()
+        .withMessage('Por favor ingrese su apellido')
+        .bail()
+        //
+        .isLength({ min:3 })
+        .withMessage('Por favor un apellido más largo'),
+        //como es la ultima no usamos bail.
+
+    body('email')
+        .notEmpty()
+        .withMessage('Por favor ingrese su email')
+        .bail()
+        //
+        .isEmail()
+        .withMessage('Por favor ingrese una dirección correcta'),
+        //como es la ultima no usamos bail.
+
+    body('password')
+        .notEmpty()
+        .withMessage('Por favor ingrese un password'),
+        
+        //como es la ultima no usamos bail.
+
+    body('imagen').custom((value, { req }) => {
+
+        const { file } = req
+
+        //chequea que haya cargado imagen
+
+        if (!file) {
+            // esto es como si hicieramos .withMessage('Seleccione un archivo')
+            throw new Error('Por favor ingrese una imagen')
+        }
+
+        const AVIABLE_EXTENSIONS = ['.jpg', '.jpeg', '.gif', '.png']
+        //sacamos la extension
+        const extension = path.extname(file.originalname)
+
+        if (!AVIABLE_EXTENSIONS.includes(extension)) {
+            //disparar error
+            throw new Error('Por favor ingrese un archivo que sea una imagen')
+        }
+
+        // chequea que la extension sea la correcta
+
+
+    })
+    
+    
+    ]
+
+module.exports = validationNewPlanet
+
+

4b93a679ba88ee144b0763df4eaee9c4d6559aaa
Maximiliano Zurita <maxi.zurita.seb@gmail.com>
2021-07-26 23:05:04 -0300

    Se agrega modelo de producto para base de datos

---
+++ BD/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO USERS (name, last_name) VALUES ("jose","mgb");
+INSERT INTO USERS (name, last_name) VALUES ("matias","mgb");
+INSERT INTO USERS (name, last_name) VALUES ("hernan","mgb");
+INSERT INTO USERS (name, last_name) VALUES ("julio","mgb");
+INSERT INTO USERS (name, last_name) VALUES ("carola","mgb");
+INSERT INTO USERS (name, last_name) VALUES ("ricoy","mgb");
+INSERT INTO USERS (name, last_name) VALUES ("agus","mgb");
+INSERT INTO USERS (name, last_name) VALUES ("dalton","mgb");
+
+
---
+++ BD/structure.sql
@@ -0,0 +1,55 @@
+CREATE TABLE users(id INT(10) NOT NULL AUTO_INCREMENT,
+	name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30),
+	email VARCHAR(35),
+	password VARCHAR(100),
+	cell INT(10),
+    category VARCHAR(30),
+    image VARCHAR(30),
+    PRIMARY KEY (id));
+                     
+CREATE TABLE categorys(id INT(10) NOT NULL AUTO_INCREMENT,
+	info VARCHAR(30),
+    PRIMARY KEY(id));
+    
+CREATE TABLE image_products(id INT(10) NOT NULL AUTO_INCREMENT,
+	url VARCHAR(30),
+    product_id INT(10),
+    PRIMARY KEY(id));
+
+CREATE TABLE brands(id INT(10) NOT NULL AUTO_INCREMENT,
+	name VARCHAR(30),
+    PRIMARY KEY(id));
+
+CREATE TABLE products(id INT(10) NOT NULL AUTO_INCREMENT,
+	name VARCHAR(30),
+    product_description VARCHAR(200),
+    category_id INT(10),
+    price INT(10),
+    quota INT(10),
+    image_id INT(10),
+    short_description VARCHAR(30),
+    stock INT(10),
+    sales INT(10),
+    brand_id INT(10),
+    PRIMARY KEY(id),
+    FOREIGN KEY(category_id) references categorys(id),
+    FOREIGN KEY(image_id) references image_products(id),
+    FOREIGN KEY(brand_id) references brands(id));
+    
+CREATE TABLE order_items(id INT(10) NOT NULL AUTO_INCREMENT,
+	user_id INT(10),
+    product_id INT(10),
+    quantity INT(10),
+    price INT(10),
+    ordered INT(10),
+    PRIMARY KEY(id),
+    FOREIGN KEY(product_id) references products(id),
+    FOREIGN KEY(user_id) references users(id));
+    
+CREATE TABLE orders(id INT(10) NOT NULL AUTO_INCREMENT,
+	user_id INT(10),
+    sum INT(10),
+    PRIMARY KEY(id),
+    FOREIGN KEY(user_id) references users(id));
+    

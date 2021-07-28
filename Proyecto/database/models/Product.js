6aa5d405a8d62aabcf6cf869072ee42c86d55242
Maximiliano Zurita <maxi.zurita.seb@gmail.com>
2021-07-28 20:54:03 -0300

    28/07

---
+++ Proyecto/database/models/Product.js
@@ -0,0 +1,51 @@
+module.exports = (sequelize, DataTypes) => {
+    const alias = 'Product'
+
+    const cols = {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        }, 
+        name: {
+            type: DataTypes.STRING
+        },
+        product_description: {
+            type: DataTypes.STRING,
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+        },
+        price: {
+            type: DataTypes.INTEGER,
+        },
+        quota: {
+            type: DataTypes.INTEGER,
+        },
+        image_id: {
+            type: DataTypes.INTEGER,
+        },
+        short_description: {
+            type: DataTypes.STRING
+        },
+        stock: {
+            type: DataTypes.STRING
+        },
+        sales: {
+            type: DataTypes.STRING
+        },
+        brands_id: {
+            type: DataTypes.STRING
+        },
+    }
+    
+    const config = {
+        underscored: true, /* te busca en snake case las columnas */
+        timestamps: false, /* no va a buscar las columnas de timestamps */
+        tableName: 'products'
+    }
+    
+    const ProductModel = sequelize.define(alias, cols, config)
+
+    return ProductModel
+};
--- Proyecto/database/models/user.js
+++
@@ -1,7 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-    const alias = ‘m’
-    const columns = {}
-    const config = {}
-    const Model = sequelize.define(alias, columns, config);
-    return Model;
-    }

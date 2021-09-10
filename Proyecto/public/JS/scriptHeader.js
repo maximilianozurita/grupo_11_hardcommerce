c2bd874e753cf197690e1e44b0a1a1fbd2873daf
Jeremías <jere.menacho@gmail.com>
2021-09-10 19:13:12 -0300

    boton busqueda

---
+++ Proyecto/public/JS/scriptHeader.js
@@ -0,0 +1,6 @@
+const searchItem = document.querySelector(".iconoSearch");
+searchItem.addEventListener("click", function(event){
+    console.log("hiciste click");
+    event.preventDefault();
+})
+

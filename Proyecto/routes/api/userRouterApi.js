3d7b659b588114d26294765aa1681b0a60077e51
martin3868 <mmila83@hotmail.com>
2021-09-12 20:16:22 -0300

    user-api

--- Proyecto/routes/api/userRouterApi.js
+++ Proyecto/routes/api/userRouterApi.js
@@ -6,5 +6,8 @@ const userController=require("../../controllers/api/userControllerApi")
 
 apiRoutes.post('/hasEmail', userController.checkEmail)
 
+apiRoutes.get('/', userController.users)
+apiRoutes.get('/:id', userController.detail)
+
 
 module.exports = apiRoutes;
